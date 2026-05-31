b1924614d07fb2d0a92fff0de1d07b062be13dbd
Henry Wrightson <henry.wrightson@studiobluesoftware.co.uk>
2026-05-31 18:38:52 +0100

    Added gh-pages for deployment to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web-react-dos2-recipe-search/',
+
   plugins: [react(), tailwindcss()],
 
   resolve: {
