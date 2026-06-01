37a2abcf216bd3e8aafab5eb967903f45fb52377
Henry Wrightson <henry.wrightson@studiobluesoftware.co.uk>
2026-06-01 13:37:59 +0100

    Fix BrowserRouter base

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/web-react-dos2-recipe-search'>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
